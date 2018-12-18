2c5524fb09b3900453b3c9f3cac200a078edb40e
Tech-A06 <hibah28@live.co.uk>
2018-12-18 22:12:19 +0000

    added the operations for each case statement

--- app.rb
+++ app.rb
@@ -1,15 +1,24 @@
 def basic_calc
-  case sign
+print "Enter your first number: "
+num1 = gets.chomp.to_f
+print "Enter your second number: "
+num2 = gets.chomp.to_f
+puts "+ Addition, - Subtraction, * Multiplication, / Division"
+sign = gets.chomp
+num3 = 0 
+
+case sign
   when "+"
     num3=num1+num2
+    puts "#{num3}"
   when "-" 
-    num3= num1-num2
+    puts "#{num3= num1-num2}"
   when "*"
-    num3=num1*num2
+    puts "#{num3=num1*num2}"
   when "/"
-    num3=num1/num2
+   puts "#{num3=num1/num2}"
   else
-    "You gave me #{x} -- I have no idea what to do with that."
+   puts "You gave me #{sign} -- I have no idea what to do with that."
   end
 end
 
@@ -17,10 +26,12 @@ def advanced_calc
   cas
 end
 
-
-
-if false
+puts "Please enter B for basic calculator or A for Advanced Caclulator"
+calc_option = gets.chomp.downcase
+if (calc_option === "b")
   basic_calc
-else
+elsif (calc_option === "a")
   advanced_calc
+else
+  puts "#{calc_option} is not an option"
 end
